1b282ccfa4b19062aab56b377b620be3d29af15d
A1an <387805107@qq.com>
2023-03-23 23:22:35 +0800

    修复新增用户角色会置空的bug

--- server/api/v1/system/sys_user.go
+++ server/api/v1/system/sys_user.go
@@ -152,11 +152,16 @@ func (b *BaseApi) Register(c *gin.Context) {
 		return
 	}
 	var authorities []system.SysAuthority
-	for _, v := range r.AuthorityIds {
-		authorities = append(authorities, system.SysAuthority{
-			AuthorityId: v,
-		})
+	err = global.GVA_DB.Find(&authorities, r.AuthorityIds).Error
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
 	}
+	//for _, v := range r.AuthorityIds {
+	//	authorities = append(authorities, system.SysAuthority{
+	//		AuthorityId: v,
+	//	})
+	//}
 	user := &system.SysUser{Username: r.Username, NickName: r.NickName, Password: r.Password, HeaderImg: r.HeaderImg, AuthorityId: r.AuthorityId, Authorities: authorities, Enable: r.Enable, Phone: r.Phone, Email: r.Email, EmployeeNo: r.EmployeeNo}
 	userReturn, err := userService.Register(*user)
 	if err != nil {
